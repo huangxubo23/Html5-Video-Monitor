12750ccfdced76eafb9ebeea701f9f94d8d030da
huangxubo23 <732150595@qq.com>
2016-11-09 19:48:28 +0800

    Add Koa.js logger

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+/**
+ * Module dependencies.
+ */
+
 const koa = require('koa');
 const path = require('path');
+const logger = require('koa-logger');
 let app = module.exports = koa();
 
 const config = require('./config');
+
+// logger middleware
+app.use(logger());
 
 app.use(function *(){
     this.body = 'Hello World!';
